607ac6be59559f6c412fc80e364c93824e573e07
Daniela <danihmello06@gmail.com>
2022-09-04 19:26:59 -0300

    separating routes and methods

--- api/server.py
+++ api/server.py
@@ -1,19 +1,22 @@
+from encodings import utf_8
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from json import dumps
+from scrapingPanelinha import *
 
 app = Flask(__name__)
 
 @app.route('/search')
 def do_search():
     palavra = request.args.get('receita')
-    
-    return "hello world"
+    olar = getSearchResult(palavra)
+    return olar
 
 @app.route('/recipe')
 def open_recipe():
-    
-    return
+    slug = request.args.get('receita')
+    olar = getRecipe(slug)
+    return olar
 
 
 
